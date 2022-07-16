d44bfb98da3146c4ba2f6e3b0e49424c7e234ce0
bpfox77 <56087137+bpfox77@users.noreply.github.com>
2022-07-16 16:55:04 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
+import { Web3Provider } from '@ethersproject/providers';
+
+import App from './App';
+
+const getLibrary = (provider) => {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 8000; // frequency provider is polling
+  return library;
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <StrictMode>
+    <ChakraProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <App />
+      </Web3ReactProvider>
+    </ChakraProvider>
+  </StrictMode>,
+  rootElement
+);
